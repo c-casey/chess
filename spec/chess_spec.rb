35af0a5a5c3ea58939e1e478285e3a2749260f9f
Christian Casey <christian.b.casey@gmail.com>
2022-10-07 10:01:34 -0700

    Replace Rook's search tests with MoveList tests

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -6,109 +6,117 @@ require_relative "../lib/chess.rb"
 describe Board do
 end
 
-describe Rook do
+describe MoveList do
   let(:board) { Board.new }
+  subject(:move_list) { described_class.new(board) }
 
-  describe "#search_up" do
-    subject(:rook) { described_class.new([0, 0], :white) }
+  describe "#search" do
 
-    context "when there are no pieces above" do
-      it "returns all the squares above" do
-        file_full = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
-        result = rook.search_up(board)
-        expect(result).to eql(file_full)
+    context "when searching upward" do
+      let(:start_square) { [0, 0] }
+      let(:search_lambda) { ->(a, b) { [a, b + 1] } }
+
+      context "when there are no pieces above" do
+        it "returns all the squares above" do
+          file_full = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_full)
+        end
+      end
+
+      context "when there is a piece above" do
+        let(:piece) { Piece.new([0, 6], :white, move_list) }
+
+        before do
+          board.state[0][6] = piece
+        end
+
+        it "returns the squares up to the piece" do
+          file_upto = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_upto)
+        end
       end
     end
 
-    context "when there is a friendly piece above" do
-      let(:friendly_piece) { described_class.new([0, 6], :white) }
+    context "when searching downward" do
+      let(:start_square) { [0, 7] }
+      let(:search_lambda) { ->(a, b) { [a, b - 1] } }
 
-      before do
-        board.state[0][6] = friendly_piece
+      context "when there are no pieces below" do
+        it "returns all the squares below" do
+          file_full = [[0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_full)
+        end
       end
 
-      it "returns the file up to the friendly piece" do
-        file_upto = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5]]
-        result = rook.search_up(board)
-        expect(result).to eql(file_upto)
-      end
-    end
-  end
+      context "when there is a piece below" do
+        let(:piece) { Piece.new([0, 1], :black, move_list) }
 
-  describe "#search_down" do
-    subject(:rook) { described_class.new([0, 7], :white) }
+        before do
+          board.state[0][1] = piece
+        end
 
-    context "when there are no pieces below" do
-      it "returns all the squares below" do
-        file_full = [[0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]
-        result = rook.search_down(board)
-        expect(result).to eql(file_full)
-      end
-    end
-
-    context "when there is a friendly piece below" do
-      let(:friendly_piece) { described_class.new([0, 1], :white) }
-
-      before do
-        board.state[0][1] = friendly_piece
-      end
-
-      it "returns the file up to the friendly piece" do
-        file_upto = [[0, 6], [0, 5], [0, 4], [0, 3], [0, 2]]
-        result = rook.search_down(board)
-        expect(result).to eql(file_upto)
-      end
-    end
-  end
-
-  describe "#search_left" do
-    subject(:rook) { described_class.new([7, 0], :white) }
-
-    context "when there are no pieces leftward" do
-      it "returns all the squares leftward" do
-        file_full = [[6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]]
-        result = rook.search_left(board)
-        expect(result).to eql(file_full)
+        it "returns the squares up to the piece" do
+          file_upto = [[0, 6], [0, 5], [0, 4], [0, 3], [0, 2]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_upto)
+        end
       end
     end
 
-    context "when there is a friendly piece leftward" do
-      let(:friendly_piece) { described_class.new([1, 0], :white) }
+    context "when searching leftward" do
+      let(:start_square) { [7, 0] }
+      let(:search_lambda) { ->(a, b) { [a - 1, b] } }
 
-      before do
-        board.state[1][0] = friendly_piece
+      context "when there are no pieces leftward" do
+        it "returns all the squares leftward" do
+          file_full = [[6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_full)
+        end
       end
 
-      it "returns the file up to the friendly piece" do
-        file_upto = [[6, 0], [5, 0], [4, 0], [3, 0], [2, 0]]
-        result = rook.search_left(board)
-        expect(result).to eql(file_upto)
+      context "when there is a piece leftward" do
+        let(:piece) { Piece.new([1, 0], :white, move_list) }
+
+        before do
+          board.state[1][0] = piece
+        end
+
+        it "returns the squares up to the piece" do
+          file_upto = [[6, 0], [5, 0], [4, 0], [3, 0], [2, 0]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_upto)
+        end
       end
     end
-  end
 
-  describe "#search_right" do
-    subject(:rook) { described_class.new([0, 0], :white) }
+    context "when searching rightward" do
+      let(:start_square) { [0, 0] }
+      let(:search_lambda) { ->(a, b) { [a + 1, b] } }
 
-    context "when there are no pieces rightward" do
-      it "returns all the squares rightward" do
-        file_full = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
-        result = rook.search_right(board)
-        expect(result).to eql(file_full)
-      end
-    end
-
-    context "when there is a friendly piece rightward" do
-      let(:friendly_piece) { described_class.new([7, 0], :white) }
-
-      before do
-        board.state[7][0] = friendly_piece
+      context "when there are no pieces rightward" do
+        it "returns all the squares rightward" do
+          file_full = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_full)
+        end
       end
 
-      it "returns the file up to the friendly piece" do
-        file_upto = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0]]
-        result = rook.search_right(board)
-        expect(result).to eql(file_upto)
+      context "when there is a piece rightward" do
+        let(:piece) { Piece.new([7, 0], :white, move_list) }
+
+        before do
+          board.state[7][0] = piece
+        end
+
+        it "returns the squares up to the piece" do
+          file_upto = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0]]
+          result = move_list.search(start_square, search_lambda)
+          expect(result).to eql(file_upto)
+        end
       end
     end
   end
