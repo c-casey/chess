7ce7a66689aaa2215e12fb21eb296a7cd079b35b
Christian Casey <christian.b.casey@gmail.com>
2022-10-07 16:52:14 -0700

    Add valid_move tests for Bishop and Rook

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -6,12 +6,51 @@ require_relative "../lib/chess.rb"
 describe Board do
 end
 
+describe Rook do
+  let(:piece) { instance_double("Piece") }
+  let(:board) { Board.new }
+  let(:move_list) { MoveList.new(board) }
+  subject(:rook) { described_class.new([4, 4], :white, move_list) }
+
+  describe "#valid_moves" do
+    before do
+      board.state[6][4] = piece
+      board.state[4][2] = piece
+    end
+
+    it "returns all legal movements for a given position" do
+      legal_moves = [[4, 5], [4, 6], [4, 7], [0, 4], [1, 4], [2, 4], [3, 4], [5, 4], [4, 3]].sort
+      result = rook.valid_moves.sort
+      expect(result).to eql(legal_moves)
+    end
+  end
+end
+
+describe Bishop do
+  let(:piece) { instance_double("Piece") }
+  let(:board) { Board.new }
+  let(:move_list) { MoveList.new(board) }
+  subject(:bishop) { described_class.new([4, 4], :white, move_list) }
+
+  describe "#valid_moves" do
+    before do
+      board.state[2][2] = piece
+      board.state[7][1] = piece
+    end
+
+    it "returns all legal movements for a given position" do
+      legal_moves = [[3, 3], [3, 5], [2, 6], [1, 7], [5, 5], [6, 6], [7, 7], [5, 3], [6, 2]].sort
+      result = bishop.valid_moves.sort
+      expect(result).to eql(legal_moves)
+    end
+  end
+end
+
 describe MoveList do
   let(:board) { Board.new }
   subject(:move_list) { described_class.new(board) }
 
   describe "#search" do
-
     context "when searching upward" do
       let(:start_square) { [0, 0] }
       let(:search_lambda) { ->(a, b) { [a, b + 1] } }
@@ -116,6 +155,49 @@ describe MoveList do
           file_upto = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0]]
           result = move_list.search(start_square, search_lambda)
           expect(result).to eql(file_upto)
+        end
+      end
+    end
+  end
+
+  describe "#attacks" do
+    context "when searching upward" do
+      let(:start_square) { [0, 0] }
+      let(:colour) { :white }
+      let(:search_lambda) { ->(a, b) { [a, b + 1] } }
+
+      context "when there is no enemy" do
+        it "returns nil" do
+          result = move_list.attacks(start_square, colour, search_lambda)
+          expect(result).to eql(nil)
+        end
+      end
+
+      context "when there is an enemy" do
+        let(:enemy) { Piece.new([0, 5], :black, move_list) }
+
+        before do
+          board.state[0][5] = enemy
+        end
+
+        it "returns the coords of the enemy" do
+          result = move_list.attacks(start_square, colour, search_lambda)
+          expect(result).to eql([0, 5])
+        end
+      end
+
+      context "when there is an enemy behind an ally" do
+        let(:enemy) { Piece.new([0, 5], :black, move_list) }
+        let(:ally) { Piece.new([0, 4], :white, move_list) }
+
+        before do
+          board.state[0][5] = enemy
+          board.state[0][4] = ally
+        end
+
+        it "returns nil" do
+          result = move_list.attacks(start_square, colour, search_lambda)
+          expect(result).to eql(nil)
         end
       end
     end
